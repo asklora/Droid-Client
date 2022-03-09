d3025968c6911f3bbd945365dfbcfba4f87c29a9
WilliamG-LORA <william.gazeley@loratechai.com>
2022-03-10 04:36:38 +0800

    Added Bistream for create bots

--- src/DroidRpc/grpc_interface/bot_pb2_grpc.py
+++ src/DroidRpc/grpc_interface/bot_pb2_grpc.py
@@ -2,7 +2,7 @@
 """Client and server classes corresponding to protobuf-defined services."""
 import grpc
 
-import bot_pb2 as bot__pb2
+from . import bot_pb2 as bot__pb2
 
 
 class EchoStub(object):
