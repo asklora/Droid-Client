bcba736fdf1efcb520d546991fd3ab7e2d2822ca
redloratech <rede.akbar@loratechai.com>
2022-06-15 20:59:01 +0700

    update packages

--- setup.py
+++ setup.py
@@ -5,23 +5,22 @@ from setuptools import find_packages
 with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-with open('requirements.txt', "r", encoding="utf-8") as f:
+with open("requirements.txt", "r", encoding="utf-8") as f:
     required = f.read().splitlines()
 
 setuptools.setup(
-    name = 'DroidRpc',
-    version='1.2.0',
-    description = 'Python client for connecting to LORA Technologies\' bot services.',
+    name="DroidRpc",
+    version="1.3.2",
+    description="Python client for connecting to LORA Technologies' bot services.",
     long_description=long_description,
-    long_description_content_type = 'text/markdown',
-    url = 'https://asklora.ai',
-    license='MIT',
+    long_description_content_type="text/markdown",
+    url="https://asklora.ai",
+    license="MIT",
     install_requires=required,
-    author = 'LORA Tech',
-    author_email = 'asklora@loratechai.com',
-    package_dir = {
-            '': 'src'},
-    packages=find_packages(where='src'),
+    author="LORA Tech",
+    author_email="asklora@loratechai.com",
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     include_package_data=True,
-    zip_safe = False
+    zip_safe=False,
 )
